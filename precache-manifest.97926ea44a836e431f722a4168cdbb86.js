1f3aec1419b448cd433d19c845ce5bc3e5a98079
mahesh-block <maheshbabu.nlp@gmail.com>
2018-12-07 14:20:16 -0500

    Updates

---
+++ precache-manifest.97926ea44a836e431f722a4168cdbb86.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "faf4419cfe9e469da9461e8e3168c3ac",
+    "url": "/portfolio/static/media/bitcoin-node.faf4419c.jpg"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "e380ab7fe2262f93cb75",
+    "url": "/portfolio/static/js/main.e380ab7f.chunk.js"
+  },
+  {
+    "revision": "2bd269d08d1b62264176",
+    "url": "/portfolio/static/js/1.2bd269d0.chunk.js"
+  },
+  {
+    "revision": "e380ab7fe2262f93cb75",
+    "url": "/portfolio/static/css/main.d701c44a.chunk.css"
+  },
+  {
+    "revision": "2bd269d08d1b62264176",
+    "url": "/portfolio/static/css/1.710695ad.chunk.css"
+  },
+  {
+    "revision": "f92b5cf1e9f047d1e86ddd020028c9e3",
+    "url": "/portfolio/index.html"
+  }
+];
